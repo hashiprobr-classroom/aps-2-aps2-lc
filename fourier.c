1694656a51177d288e40c5989a018f95b9b410ab
lennywajcberg <lennyw@al.insper.edu.br>
2026-03-08 17:57:17 -0300

    Ex2

--- fourier.c
+++ fourier.c
@@ -35,7 +35,52 @@ void nft_inverse(complex t[], complex s[], int n) {
     normalize(s, n);
 }
 
+void separa_pares(complex s[], complex s_par[], int n){
+    for (int i = 0; i < n/2; i++) { // i é o índice de s_par, i*2 é o índice de s
+        s_par[i] =  s[i * 2];  //ex: s_par[1] = s[2], só os pares
+    }
+    return;
+}
+
+void separa_impares(complex s[], complex s_impar[], int n){
+    for (int i = 0; i < n/2; i++){
+        s_impar[i] = s[i * 2 + 1];
+    }
+    return;
+}
+
 void fft(complex s[], complex t[], int n, int sign) {
+    if (n == 1){
+        t[0] = s[0];
+        return;
+    }
+    complex s_par[n/2];
+    complex s_impar[n/2];
+    complex E[n/2];
+    complex O[n/2];
+    
+    separa_pares(s, s_par, n);
+    separa_impares(s, s_impar, n);
+    
+    fft(s_par, E, n/2, sign);
+    fft(s_impar, O, n/2, sign);
+
+    for (int k  = 0; k < n/2; k ++){
+        double x = sign * 2 * PI * k/n;
+        complex w;
+        w.a = cos(x);
+        w.b = sin(x);
+
+      
+        double wO_a = w.a * O[k].a - w.b * O[k].b;
+        double wO_b = w.a * O[k].b + w.b * O[k].a;
+
+        t[k].a = E[k].a + wO_a;
+        t[k].b = E[k].b + wO_b;
+
+        t[k + n/2].a = E[k].a - wO_a;
+        t[k + n/2].b = E[k].b - wO_b;
+    }   
 }
 
 void fft_forward(complex s[], complex t[], int n) {
@@ -48,10 +93,64 @@ void fft_inverse(complex t[], complex s[], int n) {
 }
 
 void fft_forward_2d(complex matrix[MAX_SIZE][MAX_SIZE], int width, int height) {
+    complex in[MAX_SIZE];
+    complex out[MAX_SIZE];
+
+    for (int c = 0; c < width; c++) {
+        for (int l = 0; l < height; l++) {
+            in[l] = matrix[l][c];
+        }
+
+        fft_forward(in, out, height);
+
+        for (int l = 0; l < height; l++) {
+            matrix[l][c] = out[l];
+        }
+    }
+
+    for (int l = 0; l < height; l++) {
+        for (int c = 0; c < width; c++) {
+            in[c] = matrix[l][c];
+        }
+
+        fft_forward(in, out, width);
+
+        for (int c = 0; c < width; c++) {
+            matrix[l][c] = out[c];
+        }
+    }
 }
 
+
 void fft_inverse_2d(complex matrix[MAX_SIZE][MAX_SIZE], int width, int height) {
+     complex in[MAX_SIZE];
+    complex out[MAX_SIZE];
+
+    for (int l = 0; l < height; l++) {
+        for (int c = 0; c < width; c++) {
+            in[c] = matrix[l][c];
+        }
+
+        fft_inverse(in, out, width);
+
+        for (int c = 0; c < width; c++) {
+            matrix[l][c] = out[c];
+        }
+    }
+
+    for (int c = 0; c < width; c++) {
+        for (int l = 0; l < height; l++) {
+            in[l] = matrix[l][c];
+        }
+
+        fft_inverse(in, out, height);
+
+        for (int l = 0; l < height; l++) {
+            matrix[l][c] = out[l];
+        }
+    }
 }
+    
 
 void filter(complex input[MAX_SIZE][MAX_SIZE], complex output[MAX_SIZE][MAX_SIZE], int width, int height, int flip) {
     int center_x = width / 2;
